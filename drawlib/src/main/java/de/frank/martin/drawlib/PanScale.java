fde8791481c8d2b57f8358ed48f45b6f236599da
martin.frank <martin.frank.private@gmail.com>
2018-10-23 11:47:02 +0200

    changed precision to double; added getter

--- drawlib/src/main/java/de/frank/martin/drawlib/PanScale.java
+++ drawlib/src/main/java/de/frank/martin/drawlib/PanScale.java
@@ -4,35 +4,47 @@ package de.frank.martin.drawlib;
  * this interface provides a simple drawing method. Drawing is platform
  * dependent and therefore its hard to write platform independent software. This
  * interface describes a simple approach to handle different implementations.
- * 
- * @author martinFrank
  *
+ * @author martinFrank
  */
 public interface PanScale {
 
-	/**
-	 * Draws this object onto the graphics object at the given position. the
-	 * graphics can be anything, a Canvas or a Graphics or whatever. The
-	 * developer is responsible to cast the object in a proper class.
-	 * 
-	 * @param graphics - an graphic object on which can be drawn
-	 * @param xOff position where on the graphics is drawn
-	 * @param yOff position where on the graphics is drawn
-	 */
-	void draw(Object graphics, int xOff, int yOff);
+    /**
+     * Draws this object onto the graphics object at the given position. the
+     * graphics can be anything, a Canvas or a Graphics or whatever. The
+     * developer is responsible to cast the object in a proper class.
+     *
+     * @param graphics - an graphic object on which can be drawn
+     */
+    void draw(Object graphics);
 
-	/**
-	 * scales this object according to the factor
-	 * 
-	 * @param scale factor
-	 */
-	void scale(float scale);
+    /**
+     * scales this object according to the factor
+     *
+     * @param scale factor
+     */
+    void scale(double scale);
 
-	/**
-	 * moves this object (changes the draw position) to the destination
-	 * 
-	 * @param dx destination
-	 * @param dy destination
-	 */
-	void pan(int dx, int dy);
+    /**
+     * moves this object (changes the draw position) to the destination
+     *
+     * @param dx destination
+     * @param dy destination
+     */
+    void pan(double dx, double dy);
+
+    /**
+     * @return the applied scale factor
+     */
+    double getScale();
+
+    /**
+     * @return the x-component of the pan
+     */
+    double getPanX();
+
+    /**
+     * @return the y-component of the pan
+     */
+    double getPanY();
 }
